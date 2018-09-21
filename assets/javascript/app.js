cb531823e768aebad54d4b22077386da0f8c50b4
Jeme <aime.urquieta@outlook.com>
2018-09-21 16:59:29 -0500

    version 2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,38 @@
-
-
-
 $(document).ready(function () {
-    var topics = ["health food", "fitness", "pilates", "nutrition", "skin care", "running", "weight lifting"];
 
+    var topics = ["health food", "fitness", "pilates", "nutrition", "skin care", "running", "weight lifting", "skin care"];
 
     function getGiphyFunction() {
         var chosen=$(this).attr("data-name");
-        var jqueryURL ="https://api.giphy.com/v1/gifs/random?api_key=2hmfwLu9FCTywzkURnLFoPDqdeyUUhpU";
+        var jqueryURL ="https://api.giphy.com/v1/gifs/search?q=" +chosen+"&api_key=2hmfwLu9FCTywzkURnLFoPDqdeyUUhpU";
 
         $.ajax({
             URL: jqueryURL,
             method: "GET"
         }).then(function(response){
             console.log(response);
-            var classification=$("<p>").text("Rating"+response.data.rating).appendTo("#health");
-            var imgContainer=$("<img>").attr("src", response.images.fixed_height.url).appendTo("#health");
-        });
+          var results=data.result;
+          for(var i=0; i<results[i]; i++){
+            var classification=$("<p>").text("Rating"+results[i].rating).appendTo("#health");
+            var imgContainer=$("<img>").attr("src", results[i].images.original.url).appendTo("#health");
+          } });
     }
-
     function createButtons() {
         $("#buttons-container").empty();
         for (var i = 0; i < topics.length; i++) {
             var buttonsCreated = $("<button class='buttonC btn btn-primary m-1'>");
-            buttonsCreated.text(topics[i]).attr("data-name", topics[i]).appendTo("#buttons-container");
-
+           buttonsCreated.text(topics[i]).attr("data-name", topics[i]).appendTo("#buttons-container");
         }
 
         $("#addHealthItem").on("click", function (event) {
-
             event.preventDefault();
             var item = $("#healthAddTxt").val().trim();
             topics.push(item);
             createButtons();
-
         });
-
     }
-    createButtons();
+
     $("<body>").on("click", ".buttonC", getGiphyFunction);
         createButtons();
         getGiphyFunction();
-    
 });
