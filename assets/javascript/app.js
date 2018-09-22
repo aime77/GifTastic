cec146d1d715784e2a2fb42938d3e92051a4d3bb
Jeme <aime.urquieta@outlook.com>
2018-09-22 08:39:23 -0500

    version 3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,13 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    var topics = ["health food", "fitness", "pilates", "nutrition", "skin care", "running", "weight lifting", "skin care"];
+    var topics = ["health food", "fitness", "pilates", "nutrition", "skin care", "running", "weight lifting", "skin care", "soccer", "basketball", "volleyball"];
+  
 
-    function getGiphyFunction() {
-        var chosen=$(this).attr("data-name");
-        var jqueryURL ="https://api.giphy.com/v1/gifs/search?q=" +chosen+"&api_key=2hmfwLu9FCTywzkURnLFoPDqdeyUUhpU";
-
-        $.ajax({
-            URL: jqueryURL,
-            method: "GET"
-        }).then(function(response){
-            console.log(response);
-          var results=data.result;
-          for(var i=0; i<results[i]; i++){
-            var classification=$("<p>").text("Rating"+results[i].rating).appendTo("#health");
-            var imgContainer=$("<img>").attr("src", results[i].images.original.url).appendTo("#health");
-          } });
-    }
     function createButtons() {
         $("#buttons-container").empty();
         for (var i = 0; i < topics.length; i++) {
             var buttonsCreated = $("<button class='buttonC btn btn-primary m-1'>");
-           buttonsCreated.text(topics[i]).attr("data-name", topics[i]).appendTo("#buttons-container");
+            buttonsCreated.text(topics[i]).attr("data-name", topics[i]).appendTo("#buttons-container");
         }
 
         $("#addHealthItem").on("click", function (event) {
@@ -29,10 +15,40 @@ $(document).ready(function () {
             var item = $("#healthAddTxt").val().trim();
             topics.push(item);
             createButtons();
+            $("#healthAddTxt").clear();
         });
     }
 
-    $("<body>").on("click", ".buttonC", getGiphyFunction);
-        createButtons();
-        getGiphyFunction();
+    $(document).on("click", ".buttonC", function(){
+    $("#health").empty();  
+    var chosen = $(this).attr("data-name");
+    console.log(chosen);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
+    chosen + "&api_key=2hmfwLu9FCTywzkURnLFoPDqdeyUUhpU";    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(res) {
+        console.log(res);
+            for (var i = 0; i < res.data.length; i++) {
+                if(res.data[i].rating==="g"){
+                console.log(res.data[i]);
+                var imgContainer = $("<div class='card col-lg-2 col-md-4 col-sm-6 m-3'>");
+                var cardImage= $("<img class='card-img-top image-responsive'>");
+                cardImage.addClass("imgHeight");
+                cardImage.attr("src", res.data[i].images.original.url);
+                var cardBody=$("<div class='card-body'>");
+                var cardTitle=$("<h5 class='card-title'></h5>");
+                cardTitle.text("Rating: " + res.data[i].rating);
+                var cardRating=cardBody.add(cardTitle);
+
+                var allElements=imgContainer.append(cardImage, cardRating);
+                allElements.appendTo("#health");
+                }
+            }
+        });
+    });
+
+    createButtons();
 });
+    
